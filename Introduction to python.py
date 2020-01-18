c651a701a3d1e1a364904c6f46b7f304aff711f7
cooperfljudson <59746742+cooperfljudson@users.noreply.github.com>
2020-01-18 12:27:51 -0500

    Create Introduction to python.py
    Cooper switched to folder

---
+++ Introduction to python.py
@@ -0,0 +1,46 @@
+# Introductory document to learn python syntax
+# Variable declaration as well as introduction to the shell
+y = 7
+x = 9
+cooper = 'is the best teacher ever'
+pythonList = [23,5,'hello world',[1,2,3,4,5,'6'],873.43234324]
+myList = []
+# print(pythonList)
+# print(pythonList[2])
+# print(pythonList[3][5])
+# print(myList)
+# print(type(cooper))
+
+# I will now show how to use the shell!!
+
+# # Basic python syntax part 2
+# if x == 7:
+#     print('x is 7')
+#     if y == 9:
+#         print('y is 9')
+#     else:
+#         print('y is not 9')
+# else:
+#     print('x is not 7')
+#
+# # For loop
+# for i in range(6):
+#     print(i)
+#
+# for stuff in (cooper):
+#     print(stuff)
+
+for nums,items in enumerate(pythonList):
+    print("number of item =", nums, "  Name of item =", items)
+
+
+
+
+
+
+
+
+
+
+
+
